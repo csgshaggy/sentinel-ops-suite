8caab3adddfaf5b753db4789a3b43cbafd188442
Charlie <csgshaggy@users.noreply.github.com.>
2026-03-30 05:08:34 -0400

    Auto-sync: Mon Mar 30 05:08:34 AM EDT 2026

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Start backend in new terminal
-gnome-terminal -- bash -c "./run_backend.sh; exec bash"
+## gnome-terminal -- bash -c "./run_backend.sh; exec bash"
 
 # Start TUI in current terminal
 ./run_tui.sh
